d0cc4f6c28efac3a1d40443d845fe487b42ff663
kuco23 <nseverkar@gmail.com>
2024-08-26 13:50:15 +0200

    fix analytic queries

--- packages/fasset-indexer-core/src/analytics/analytics.ts
+++ packages/fasset-indexer-core/src/analytics/analytics.ts
@@ -2,7 +2,6 @@ import { getOrmConfig } from "../config/utils"
 import { createOrm } from "../database/utils"
 import { getVar } from "../indexer/shared"
 import { EvmLog } from "../database/entities/evm/log"
-import { AgentVault } from "../database/entities/agent"
 import { AgentVaultInfo } from "../database/entities/state/agent"
 import { CollateralReserved, MintingExecuted } from "../database/entities/events/minting"
 import { RedemptionPerformed, RedemptionRequested } from "../database/entities/events/redemption"
@@ -41,85 +40,21 @@ export class Analytics {
     return parseInt(end.value) - parseInt(start.value) + 1
   }
 
-  //////////////////////////////////////////////////////////////
-  // mintings
-
-  async totalReserved(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(cr.value_uba) as total
-      FROM collateral_reserved cr
-    `)
-    return result[0].total
-  }
-
-  async totalMinted(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(cr.value_uba) as total
-      FROM minting_executed me
-      INNER JOIN collateral_reserved cr ON me.collateral_reserved_collateral_reservation_id = cr.collateral_reservation_id
-    `)
-    return result[0].total
-  }
-
-  async totalMintingDefaulted(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(cr.value_uba) as total
-      FROM minting_payment_default mpd
-      INNER JOIN collateral_reserved cr ON mpd.collateral_reserved_collateral_reservation_id = cr.collateral_reservation_id
-    `)
-    return result[0].total
-  }
-
-  //////////////////////////////////////////////////////////////////
-  // redemptions
-
-  async totalRedemptionRequested(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(value_uba) as total
-      FROM redemption_requested
-    `)
-    return result[0].total
-  }
-
-  async totalRedeemed(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(rr.value_uba) as total
-      FROM redemption_requested rr
-      INNER JOIN redemption_performed rp ON rp.redemption_requested_request_id = rr.request_id
-    `)
-    return result[0].total
-  }
-
-  async totalRedemptionDefaulted(): Promise<bigint> {
-    const em = this.orm.em.fork()
-    const result = await em.getConnection('read').execute(`
-      SELECT SUM(rr.value_uba) as total
-      FROM redemption_requested rr
-      INNER JOIN redemption_default rd ON rd.redemption_requested_request_id = rr.request_id
-    `)
-    return result[0].total
-  }
-
   async totalRedemptionRequesters(): Promise<number> {
-    const qb = this.orm.em.qb(RedemptionRequested, 'o')
-    const result = await qb.count('o.redeemer', true).execute()
+    const qb = this.orm.em.qb(RedemptionRequested, 'r')
+    const result = await qb.count('r.redeemer', true).execute()
     return result[0].count
   }
 
   async totalCollateralReservers(): Promise<number> {
-    const qb = this.orm.em.qb(CollateralReserved, 'o')
-    const result = await qb.count('o.minter', true).execute()
+    const qb = this.orm.em.qb(CollateralReserved, 'c')
+    const result = await qb.count('c.minter', true).execute()
     return result[0].count
   }
 
   async totalLiquidators(): Promise<number> {
-    const qb = this.orm.em.qb(LiquidationPerformed, 'o')
-    const result = await qb.count('o.liquidator', true).execute()
+    const qb = this.orm.em.qb(LiquidationPerformed, 'l')
+    const result = await qb.count('l.liquidator', true).execute()
     return result[0].count
   }
 
@@ -139,71 +74,66 @@ export class Analytics {
   // agent specific (liquidation count, mint count, redeem count, percent of successful redemptions
 
   async agentCollateralReservationCount(agentAddress: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(CollateralReserved, 'o')
-    qb.select('o.collateral_reservation_id').where({ agentVault: { address: { hex: agentAddress }}})
-    const result = await qb.count('o.collateral_reservation_id').execute()
+    const qb = this.orm.em.fork().qb(CollateralReserved)
+    qb.count().where({ agentVault: { address: { hex: agentAddress }}})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async agentMintingExecutedCount(agentAddress: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(MintingExecuted, 'o')
-    qb.select('o.collateral_reserved_collateral_reservation_id')
-      .where({ collateralReserved: { agentVault: { address: { hex: agentAddress }}}})
-    const result = await qb.count('o.collateral_reserved_collateral_reservation_id').execute()
+    const qb = this.orm.em.fork().qb(MintingExecuted)
+    qb.count().where({ collateralReserved: { agentVault: { address: { hex: agentAddress }}}})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async agentRedemptionRequestCount(agentAddress: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(RedemptionRequested, 'o')
-    qb.select('o.request_id').where({ agentVault: { address: { hex: agentAddress }}})
-    const result = await qb.count('o.request_id').execute()
+    const qb = this.orm.em.fork().qb(RedemptionRequested)
+    qb.count().where({ agentVault: { address: { hex: agentAddress }}})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async agentRedemptionPerformedCount(agentAddress: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(RedemptionPerformed, 'o')
-    qb.select('o.redemption_requested_request_id')
-      .where({ redemptionRequested: { agentVault: { address: { hex: agentAddress }}}})
-    const result = await qb.count('o.redemption_requested_request_id').execute()
+    const qb = this.orm.em.fork().qb(RedemptionPerformed)
+    qb.count().where({ redemptionRequested: { agentVault: { address: { hex: agentAddress }}}})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async agentRedemptionSuccessRate(agentAddress: string): Promise<number> {
     const requested = await this.agentRedemptionRequestCount(agentAddress)
     const executed = await this.agentRedemptionPerformedCount(agentAddress)
-    return requested > 0 ? executed / requested : 0
+    return Number(requested) > 0 ? Number(executed) / Number(requested) : 0
   }
 
   async agentLiquidationCount(agentAddress: string): Promise<number> {
     const qb = this.orm.em.fork().qb(LiquidationPerformed, 'o')
-    qb.select('o.id').where({ agentVault: { address: { hex: agentAddress }}, valueUBA: { $gt: 0 } })
-    const result = await qb.count('o.id').execute()
+    qb.count().where({ agentVault: { address: { hex: agentAddress }}, valueUBA: { $gt: 0 } })
+    const result = await qb.execute()
     return result[0].count
   }
-
-  //////////////////////////////////////////////////////////////////////
-  // user specific
 
   ////////////////////////////////////////////////////////////////////
   // executor
 
   async executorRedemptionRequests(executor: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(RedemptionRequested, 'o')
-    qb.select('o.request_id').where({ executor: { hex: executor }})
-    const result = await qb.count('o.request_id').execute()
+    const qb = this.orm.em.fork().qb(RedemptionRequested)
+    qb.count().where({ executor: { hex: executor }})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async executorMintingPerformed(executor: string): Promise<number> {
-    const qb = this.orm.em.fork().qb(MintingExecuted, 'o')
-    qb.select('o.collateral_reserved_collateral_reservation_id').where({ collateralReserved: { executor: { hex: executor }}})
-    const result = await qb.count('o.collateral_reserved_collateral_reservation_id').execute()
+    const qb = this.orm.em.fork().qb(MintingExecuted)
+    qb.count().where({ collateralReserved: { executor: { hex: executor }}})
+    const result = await qb.execute()
     return result[0].count
   }
 
   async totalMintingExecutions(): Promise<number> {
-    const qb = this.orm.em.fork().qb(MintingExecuted, 'o')
-    const result = await qb.count('o.collateral_reserved_collateral_reservation_id').execute()
+    const qb = this.orm.em.fork().qb(MintingExecuted)
+    const result = await qb.count().execute()
     return result[0].count
   }
 
@@ -247,3 +177,14 @@ export class Analytics {
   }
 
 }
+
+/* import { Context } from "../context"
+import { config } from "../config/config"
+async function main() {
+  const context = await Context.create(config)
+  const analytics = new Analytics(context.orm)
+  console.log(await analytics.totalRedemptionRequesters())
+  await context.orm.close()
+}
+
+main() */
