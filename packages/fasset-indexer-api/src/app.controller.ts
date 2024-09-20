bba108827c651c3c0b83517f40fbdb3fda7a6254
kuco23 <nseverkar@gmail.com>
2024-09-20 09:00:46 +0200

    ignore .env.production in .gitleaks

--- packages/fasset-indexer-api/src/app.controller.ts
+++ packages/fasset-indexer-api/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { FAssetIndexerService } from './app.service'
 import { apiResponse, type ApiResponse } from './common/api-response'
