d232c37688fa1a9301ecc544e80797b6a1ba858d
kuco23 <nseverkar@gmail.com>
2024-11-11 19:01:39 +0100

    fix balance script

--- packages/fasset-indexer-core/src/run/run-event-indexer.ts
+++ packages/fasset-indexer-core/src/run/run-event-indexer.ts
@@ -4,40 +4,35 @@ import { EventIndexer } from "../indexer-evm/evm-indexer"
 import { ERC20Transfer } from "../database/entities/events/token"
 import { raw } from "@mikro-orm/core"
 import { TokenBalance } from "../database/entities/state/balance"
+import { EvmAddress } from "../database/entities/address"
 
 async function setTokenBalances(context: Context) {
-  /* const testAcc = '0xe2465d57a8719B3f0cCBc12dD095EFa1CC55A997'
-  const tokenAcc = '0x51B1ac96027e55c29Ece8a6fD99DdDdd01F22F6c' */
+  const testAcc = '0xe2465d57a8719B3f0cCBc12dD095EFa1CC55A997'
+  const tokenAcc = '0x51B1ac96027e55c29Ece8a6fD99DdDdd01F22F6c'
+  const testAccId = await context.orm.em.fork().findOneOrFail(EvmAddress, { hex: testAcc })
+  const tokenAccId = await context.orm.em.fork().findOneOrFail(EvmAddress, { hex: tokenAcc })
   await context.orm.em.createQueryBuilder(TokenBalance).delete().execute()
   const em = context.orm.em.fork()
   const plus = await em.createQueryBuilder(ERC20Transfer, 't')
-    .select(['a.id as user_id', 'la.id as token_id', 'a.hex as user', 'la.hex as token', raw('SUM(t.value) as value')])
-    .join('evmLog', 'l')
-    .join('t.to', 'a')
-    .join('l.address', 'la')
-    //.where({ 'a.hex': testAcc, 'la.hex': tokenAcc })
-    .groupBy(['l.address', 't.to', 'a.id', 'la.id'])
+    .select(['l.address_id as token_id', 't.to_id as user_id', raw('SUM(t.value) as value')])
+    .join('t.evmLog', 'l')
+    //.where({ 'l.address': tokenAccId, 't.to': testAccId })
+    .groupBy(['l.address', 't.to'])
     .execute()
-  for (const { token_id, user_id, token, user, value } of plus as any) {
-    //console.log('plusing token', token, 'to', user, 'value', value)
-    const balance = new TokenBalance(token_id, user_id, BigInt(value))
-    await em.persistAndFlush(balance)
-  }
-  const minus = await em.createQueryBuilder(ERC20Transfer, 't')
-    .select(['a.id as user_id', 'la.id as token_id', 'a.hex as user', 'la.hex as token', raw('SUM(t.value) as value')])
-    .join('evmLog', 'l')
-    .join('t.from', 'a')
-    .join('l.address', 'la')
-    //.where({ 'a.hex': testAcc, 'la.hex': tokenAcc })
-    .groupBy(['l.address', 't.from', 'a.hex', 'la.hex'])
-    .execute()
-  for (const { token_id, user_id, token, user, value } of minus as any) {
-    //console.log('minusing token', token, 'from', user, 'value', value)
-    let balance = await em.findOne(TokenBalance, { token: token_id, holder: user_id })
-    if (balance === null) {
-      balance = new TokenBalance(token_id, user_id, BigInt(0))
+  let i = 0
+  for (const { token_id, user_id, value } of plus as any) {
+    i += 1
+    const minus = await em.createQueryBuilder(ERC20Transfer, 't')
+      .select([raw('SUM(t.value) as value')])
+      .join('t.evmLog', 'l')
+      .where({ 'l.address': token_id, 't.from': user_id })
+      .execute()
+    const total = BigInt(value) - BigInt(minus[0]?.value || 0)
+    if (Math.floor(i % 100) == 0) {
+      console.log(i, plus.length)
     }
-    balance.amount -= BigInt(value)
+    //console.log(i++, token_id, user_id, ':', value, '+', minus[0].value, '=', total)
+    const balance = new TokenBalance(token_id, user_id, total)
     await em.persistAndFlush(balance)
   }
 }
