671da4b8c94e19c2e02487124630a585ae6b7983
kuco23 <nseverkar@gmail.com>
2024-12-02 18:24:03 +0100

    fix: change relative default address coston path to absolute

--- packages/fasset-indexer-core/src/config/contracts.ts
+++ packages/fasset-indexer-core/src/config/contracts.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import { resolve } from "path"
 
-const addressesJsonPath = resolve(process.env.ADDRESSES_JSON || '../../chain/coston.json')
+const addressesJsonPath = resolve(process.env.ADDRESSES_JSON || './packages/fasset-indexer-core/chain/coston.json')
 const addressesJson = require(addressesJsonPath)
 
 export const contracts: {
