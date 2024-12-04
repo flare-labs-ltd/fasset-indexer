cb571264e84b3c00748f0b9ce9f1cc43ad501410
kuco23 <nseverkar@gmail.com>
2024-12-04 15:18:27 +0100

    feat: check asset manager controller integrity

---
+++ packages/fasset-indexer-core/src/run/db-integrity.ts
@@ -0,0 +1,56 @@
+
+import { getVar, setVar } from "../utils"
+import { Context } from "../context/context"
+import { BLOCK_EXPLORERS, MIN_EVM_BLOCK_NUMBER_DB_KEY } from "../config/constants"
+import type { JsonRpcApiProvider } from "ethers"
+
+
+export async function ensureDatabaseIntegrity(context: Context): Promise<void> {
+  const em = context.orm.em.fork()
+  const dbchain = await getVar(em, 'chain')
+  if (dbchain == null) {
+    return await markNewDatabase(context)
+  }
+  // check chain
+  if (dbchain.value !== context.config.chain) {
+    throw new Error(`Database chain ${dbchain.value} does not match config chain ${context.config.chain}`)
+  }
+  // check asset manager controller
+  const dbamc = await getVar(em, 'asset_manager_controller')
+  const cfamc = context.getContractAddress('AssetManagerController')
+  if (dbamc != null && dbamc.value !== cfamc) {
+    throw new Error(`AssetManagerController contract addresses in database ${dbamc.value} does not match ${cfamc} from address file`)
+  }
+  // check min block number
+  const minblock = await getVar(em, MIN_EVM_BLOCK_NUMBER_DB_KEY)
+  if (minblock == null) {
+    throw new Error(`Database missing minimum block number field '${MIN_EVM_BLOCK_NUMBER_DB_KEY}'`)
+  }
+}
+
+async function markNewDatabase(context: Context): Promise<void> {
+  const envchain = context.config.chain
+  const amc = context.getContractAddress('AssetManagerController')
+  let minblock = context.config.minBlockNum ?? null
+  if (minblock == null) {
+    minblock = await getContractCreationBlock(context.provider, amc, envchain)
+  }
+  if (minblock == null) {
+    throw new Error(`Could not find min block number in .env or the creation block for AssetManagerController ${amc} on block explorer api`)
+  }
+  await context.orm.em.transactional(async em => {
+    await setVar(em, 'chain', envchain)
+    await setVar(em, 'asset_manager_controller', amc)
+    await setVar(em, MIN_EVM_BLOCK_NUMBER_DB_KEY, minblock.toString())
+  })
+}
+
+async function getContractCreationBlock(provider: JsonRpcApiProvider, address: string, chain: string): Promise<number | null> {
+  const blockExplorer = BLOCK_EXPLORERS[chain as keyof typeof BLOCK_EXPLORERS]
+  if (blockExplorer == null) return null
+  const resp = await fetch(`${blockExplorer}/api/v2/addresses/${address}`)
+  const json = await resp.json()
+  const hash = json.creation_tx_hash
+  const creation = await provider.getTransaction(hash)
+  return creation?.blockNumber ?? null
+}
