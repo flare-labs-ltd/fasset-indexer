5fe9eaf9ed26f757ff21285f7d487a7baa7c4348
kuco23 <nseverkar@gmail.com>
2024-12-04 11:35:38 +0100

    fix: api-doc global path prefix

--- packages/fasset-indexer-api/src/main.ts
+++ packages/fasset-indexer-api/src/main.ts
@@ -4,11 +4,13 @@ import { FAssetIndexerModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { apiConfig } from './config'
 
+let apiDoc = 'api-doc'
 
 async function bootstrap() {
   const app = await NestFactory.create(FAssetIndexerModule)
   if (apiConfig.rootPath != null && apiConfig.rootPath != '') {
     app.setGlobalPrefix(apiConfig.rootPath)
+    apiDoc = apiConfig.rootPath + '/' + apiDoc
   }
   const config = new DocumentBuilder()
     .setTitle('FAsset Indexer')
@@ -16,7 +18,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api-doc', app, document)
+  SwaggerModule.setup(apiDoc, app, document)
   await app.use(helmet()).listen(apiConfig.port)
 }
 
