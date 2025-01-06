ab71006d58d3feb720619767495fc6169e011908
kuco23 <nseverkar@gmail.com>
2025-01-06 14:48:14 +0100

    feat: finish race indexing, run ordinary indexer

--- packages/fasset-indexer-core/src/run/run-indexer.ts
+++ packages/fasset-indexer-core/src/run/run-indexer.ts
@@ -1,29 +1,13 @@
 import { ensureConfigIntegrity } from "./integrity"
-import { EventIndexerParallelRacePopulation } from "../indexer/migrations/indexer-parallel-race-population"
-import { EVENTS } from "../config/constants"
 import { Context } from "../context/context"
 import { logger } from "../logger"
 import { config } from "../config/config"
+import { EventIndexer } from "../indexer/indexer"
 
-const events = [
-  EVENTS.ASSET_MANAGER.REDEMPTION_TICKET_CREATED,
-  EVENTS.ASSET_MANAGER.REDEMPTION_TICKET_UPDATED,
-  EVENTS.ASSET_MANAGER.REDEMPTION_TICKET_DELETED,
-  EVENTS.ASSET_MANAGER.VAULT_COLLATERAL_WITHDRAWAL_ANNOUNCED,
-  EVENTS.ASSET_MANAGER.POOL_TOKEN_REDEMPTION_ANNOUNCED,
-  EVENTS.ASSET_MANAGER.UNDERLYING_WITHDRAWAL_ANNOUNCED,
-  EVENTS.ASSET_MANAGER.UNDERLYING_WITHDRAWAL_CONFIRMED
-]
-
-const updateName = 'redemptionTicketsAndWithdrawAnnouncments'
 
 async function runIndexer(start?: number) {
-  const allEvents = Object.values(EVENTS).map(x => Object.values(x)).flat()
   const context = await Context.create(config)
-  const indexer = new EventIndexerParallelRacePopulation(
-    context, allEvents, updateName,
-    allEvents.filter(x => !events.includes(x))
-  )
+  const indexer = new EventIndexer(context)
 
   process.on("SIGINT", async () => {
     logger.info("stopping indexer...")
@@ -32,7 +16,7 @@ async function runIndexer(start?: number) {
   })
 
   logger.info("ensuring configuration integrity...")
-  await ensureConfigIntegrity(context, updateName)
+  await ensureConfigIntegrity(context)
   logger.info(`starting FAsset ${context.chain} indexer...`)
   await indexer.run()
 }
