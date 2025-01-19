11295d368f21babd033a3b77da8e465c90068599
kuco23 <nseverkar@gmail.com>
2025-01-19 03:51:43 +0100

    fix: remove readonly for indexer attributes

--- packages/fasset-indexer-core/src/indexer/indexer.ts
+++ packages/fasset-indexer-core/src/indexer/indexer.ts
@@ -17,9 +17,9 @@ import type { Context } from '../context/context'
 
 
 export class EventIndexer {
-  readonly eventScraper: EventScraper
-  readonly eventParser: EventParser
-  readonly stateUpdater: StateUpdater
+  eventScraper: EventScraper
+  eventParser: EventParser
+  stateUpdater: StateUpdater
 
   constructor(public readonly context: Context, eventnames?: string[]) {
     this.eventScraper = new EventScraper(context)
