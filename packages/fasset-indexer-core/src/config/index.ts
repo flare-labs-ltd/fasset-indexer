239f2ee086fec3c52fbd0a33b4d64b1f66434a86
kuco23 <nseverkar@gmail.com>
2025-01-22 17:23:28 +0100

    fix: constants -> config export path

--- packages/fasset-indexer-core/src/config/index.ts
+++ packages/fasset-indexer-core/src/config/index.ts
@@ -1,2 +1,5 @@
 export { ConfigLoader } from "./config"
-export { MIN_EVM_BLOCK_NUMBER_DB_KEY, EVENTS, FASSET_LOT_SIZE } from "./constants"
+export {
+    MIN_EVM_BLOCK_NUMBER_DB_KEY, EVENTS, FASSET_LOT_SIZE,
+    MIN_DATABASE_POOL_CONNECTIONS, MAX_DATABASE_POOL_CONNECTIONS
+ } from "./constants"
