332ecc79a9ba0b97d652f0d657e139d3f55c403a
kuco23 <nseverkar@gmail.com>
2025-01-23 15:00:03 +0100

    fix: keep compatibility with old databases by omitting redemption ticket indexing

--- packages/fasset-indexer-core/src/run/run-indexer.ts
+++ packages/fasset-indexer-core/src/run/run-indexer.ts
@@ -1,25 +1,19 @@
-import { AgentVault } from "../orm/entities/agent"
 import { EventIndexer } from "../indexer/indexer"
 import { ensureConfigIntegrity } from "./integrity"
 import { ConfigLoader } from "../config/config"
 import { Context } from "../context/context"
 import { logger } from "../logger"
 import { IndexerRunner } from "../indexer/runner"
+import { EVENTS } from "../config"
 
-async function fillCollateralPoolTokens(context: Context) {
-  const em = context.orm.em.fork()
-  const agentVaults = await em.findAll(AgentVault, { populate: ['collateralPoolToken'] })
-  for (const agentVault of agentVaults) {
-    const collateralPoolToken = context.getERC20(agentVault.collateralPoolToken.hex)
-    agentVault.collateralPoolTokenSymbol = await collateralPoolToken.symbol()
-    await em.persistAndFlush(agentVault)
-  }
-}
+
+const eventnames = Object.keys(EVENTS).map(iface => Object.values(EVENTS[iface as keyof typeof EVENTS])).flat()
+  .filter(x => !x.startsWith('RedemptionTicket'))
 
 async function runIndexer(start?: number) {
   const config = new ConfigLoader()
   const context = await Context.create(config)
-  const indexer = new EventIndexer(context)
+  const indexer = new EventIndexer(context, eventnames)
   const runner = new IndexerRunner(indexer, 'event indexer')
 
   process.on("SIGINT", async () => {
@@ -27,8 +21,6 @@ async function runIndexer(start?: number) {
     await context.orm.close()
     process.exit(0)
   })
-
-  await fillCollateralPoolTokens(context)
 
   logger.info("ensuring configuration integrity...")
   await ensureConfigIntegrity(context)
